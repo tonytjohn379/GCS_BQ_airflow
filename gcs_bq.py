05f26429be003a7914203b411239f404ca0c254c
Tony T John <tonytjohn379@gmail.com.com>
2026-09-23 11:32:41 +0530

    Initial commit

---
+++ gcs_bq.py
@@ -0,0 +1,44 @@
+# ######  GCS to BigQuey  ######
+
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+# DAG default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+# DAG definition
+with DAG(
+    dag_id='load_gcs_to_bq',
+    default_args=default_args,
+    description='Load a CSV file from GCS to BigQuery',
+    schedule=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=['bigquery', 'gcs', 'csv'],
+) as dag:
+
+    # Task to load CSV from GCS to BigQuery
+    load_csv_to_bigquery = GCSToBigQueryOperator(
+        task_id='load_csv_to_bq',
+        bucket='disease_global',  
+        source_objects=['disease_data_global.csv'],
+        destination_project_dataset_table='tony1-508206.staging_dataset.global_data',  # Replace with your project, dataset, and table name
+        source_format='CSV', 
+        allow_jagged_rows=True,
+        ignore_unknown_values=True,
+        write_disposition='WRITE_TRUNCATE',  # Options: WRITE_TRUNCATE, WRITE_APPEND, WRITE_EMPTY
+        skip_leading_rows=1,  # Skip header row
+        field_delimiter=',',  # Delimiter for CSV, default is ','
+        autodetect=True,  # Automatically infer schema from the file
+        #google_cloud_storage_conn_id='google_cloud_default',  # Replace with your Airflow GCP connection ID if not default
+        #bigquery_conn_id='google_cloud_default',  # Replace with your Airflow BigQuery connection ID if not default
+    )
+
+    
